9fb0271ea4e301da6aaa58e72b91785da65d1568
gilliangibson <gilliangibson@seattleacademy.org>
2015-05-22 09:00:24 -0700

    calibrate center.js

--- center.js
+++ center.js
@@ -4,10 +4,10 @@ console.log(ThunderConnector);
 ThunderConnector.connect();
 function center(){
 setTimeout(function(){ThunderConnector.command('right');},0);
-setTimeout(function(){ThunderConnector.command('down');},6000);
-setTimeout(function(){ThunderConnector.command('left');},7000);
-setTimeout(function(){ThunderConnector.command('up');},10000);
-setTimeout(function(){ThunderConnector.command('stop');},10500);
+setTimeout(function(){ThunderConnector.command('down');},6500);
+setTimeout(function(){ThunderConnector.command('left');},8000);
+setTimeout(function(){ThunderConnector.command('up');},11050);
+setTimeout(function(){ThunderConnector.command('stop');},11850);
 //setTimeout(function(){ThunderConnector.command('fire');},10500);
 
 
